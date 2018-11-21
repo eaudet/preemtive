b6af894cf5fb01cf454c2ddeb8c1e36b0bba4c7a
erickaudet <eaudet@jarics.com>
2018-11-21 09:23:32 -0500

    initial commit

---
+++ src/main/java/com/jarics/preemtive/OutputMessage.java
@@ -0,0 +1,9 @@
+package com.jarics.preemtive;
+
+public class OutputMessage extends Message {
+
+
+  public OutputMessage(String from, String text, String time) {
+    super(from, text, time);
+  }
+}
---
+++ src/main/java/com/jarics/preemtive/WebSocketController.java
@@ -0,0 +1,26 @@
+package com.jarics.preemtive;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+  private final SimpMessagingTemplate template;
+
+  @Autowired
+  WebSocketController( SimpMessagingTemplate template){
+    this.template = template;
+  }
+
+  @MessageMapping("/send/message")
+  public void onReceiveMessage(String message){
+    this.template.convertAndSend("/chat",
+        new SimpleDateFormat("HH:mm:ss").format(new Date())+"- "+message);
+  }
+
+}
